951e799aabddeebe88059b363f0a9923ec52d667
Davide Airaghi <airaghi.davide@gmail.com>
2021-10-17 16:14:03 +0200

    Update financial_inclusion_app.py
    add dataframe melting

--- financial_inclusion_app.py
+++ financial_inclusion_app.py
@@ -47,8 +47,8 @@ job_type = st.sidebar.selectbox('Job type',('Self employed','Informally employed
 
 features = {'country': country,'location_type': location_type,'cellphone_access': cellphone_access,'household_size': household_size,'age_of_respondent': age_of_respondent,'gender_of_respondent' : gender_of_respondent,'relationship_with_head' : relationship_with_head,'marital_status' : marital_status,'education_level': education_level,'job_type' : job_type}
 
-features_df  = pd.DataFrame([features])
-st.write(features_df)
+features_df = pd.DataFrame([features])
+st.dataframe(pd.melt(features_df,value_vars=['country','location_type','cellphone_access','household_size','age_of_respondent','gender_of_respondent','relationship_with_head','marital_status','education_level','job_type']))
 
 if st.button('Predict'):
     
