a3036ad99a0508a1773a4287972b9500a914de3e
Davide Airaghi <airaghi.davide@gmail.com>
2021-10-17 17:47:22 +0200

    Update financial_inclusion_app.py

--- financial_inclusion_app.py
+++ financial_inclusion_app.py
@@ -50,7 +50,7 @@ features = {'country': country,'location_type': location_type,'cellphone_access'
 df = pd.melt(pd.DataFrame([features]),value_vars=['country','location_type','cellphone_access','household_size','age_of_respondent','gender_of_respondent','relationship_with_head','marital_status','education_level','job_type'])
 
 features_df = pd.DataFrame([features])
-new_df = pd.DataFrame(df)
+new_df = pd.DataFrame([df])
 
 st.write(new_df)
 
